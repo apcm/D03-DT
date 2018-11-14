b6302b5e3398be8ff182ee4e3dcb25aca7047e2b
josmunpen <josemamup@gmail.com>
2018-11-14 10:41:38 +0100

    Authority cambiada

--- Acme-Handy-Worker/src/main/java/security/Authority.java
+++ Acme-Handy-Worker/src/main/java/security/Authority.java
@@ -39,6 +39,9 @@ public class Authority implements GrantedAuthority {
 
 	public static final String	ADMIN		= "ADMIN";
 	public static final String	CUSTOMER	= "CUSTOMER";
+	public static final String	HANDYWORKER	= "HANDYWORKER";
+	public static final String	SPONSOR		= "SPONSOR";
+	public static final String	REFEREE		= "REFEREE";
 
 	// Attributes -------------------------------------------------------------
 
@@ -46,7 +49,7 @@ public class Authority implements GrantedAuthority {
 
 
 	@NotBlank
-	@Pattern(regexp = "^" + Authority.ADMIN + "|" + Authority.CUSTOMER + "$")
+	@Pattern(regexp = "^" + Authority.ADMIN + "|" + Authority.CUSTOMER + "|" + Authority.HANDYWORKER + "|" + Authority.REFEREE + "|" + Authority.SPONSOR + "$")
 	@Override
 	public String getAuthority() {
 		return this.authority;
@@ -68,6 +71,14 @@ public class Authority implements GrantedAuthority {
 
 		authority = new Authority();
 		authority.setAuthority(Authority.CUSTOMER);
+		result.add(authority);
+
+		authority = new Authority();
+		authority.setAuthority(Authority.REFEREE);
+		result.add(authority);
+
+		authority = new Authority();
+		authority.setAuthority(Authority.SPONSOR);
 		result.add(authority);
 
 		return result;
